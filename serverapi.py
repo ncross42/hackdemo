36f1f250966caf3bb8efedc56caa03f54f761c32
HeeWon Lee <ncross42@gmail.com>
2015-12-19 19:41:47 +0900

    added process : /user_list fixed process : /voted_list

--- serverapi.py
+++ serverapi.py
@@ -1,6 +1,9 @@
 #-*- coding: utf-8 -*-
 import sqlite3
 import os
+from var_dump import var_dump
+import json
+
 #import logging
 #from logging.handlers import RotatingFileHandler
 
@@ -28,21 +31,48 @@ def index():
 
 @app.route('/voted_list/')
 def voted_list():
-    f = open('s/votedlist.json')
-#    js = json.loads(f.read())
-    str = f.read();
-    f.close()
-    return str
+    voted_obj = {"votes":[]}
+    voted_json = '{"votes":['
+    try:
+        c = g.db.cursor()
+        # 's/votedlist.json'
+        sql = "SELECT name, level, val FROM vote JOIN user USING (user_id)"
+        # get old_val
+        c.execute(sql)
+        rows = c.fetchall()
+        # execute new_val
+        if rows is None :
+            return sql
+        else :
+            voted_list = []
+            for row in rows :
+                voted_obj['votes'].append( {'name':row[0],'level':row[1],'value':row[2]} )
+                voted_list.append ( '\n\t{ "name":"%s", "level":"%s", "value":"%s" }' % row )
 
-@app.route('/voting/<user>/<value>')
-def voting(user,value,issue=ISSUE):
+            voted_json += ','.join(voted_list)
+            voted_json += "\n]}"
+            return voted_json
+            #return json.dumps(voted_obj)
+
+    except sqlite3.Error as e:
+        return "An DB error occurred:", e.args[0]
+
+@app.route('/voting')
+def voting():
+
+    user = request.args.get('name', '')
+    value = request.args.get('value', '')
+    #issue = request.args.get('subject', '')
+    issue = ISSUE
+    #app.logger.info('name: %s, value: %s, subject: %s', user, value, issue)
+
     new_val = int(value)
     try:
         c = g.db.cursor()
         # get issue
         c.execute("SELECT issue_id FROM issue WHERE name='%s'" % (issue) )
         row = c.fetchone()
-        if row == None :
+        if row is None :
             raise Exception('Unkown Issue name : ' + issue )
         issue_id = row[0]
         # get user
@@ -51,27 +81,53 @@ def voting(user,value,issue=ISSUE):
         else :
             c.execute("SELECT user_id FROM user WHERE name='%s'" % (user) )
             row = c.fetchone()
-            if row == None :
+            if row is None :
                 raise Exception('Unkown user name : ' + user )
             user_id = row[0]
         # get old_val
         c.execute("SELECT val FROM vote WHERE issue_id=%d AND user_id=%d" % (issue_id,user_id) )
         row = c.fetchone()
         # execute new_val
-        if row == None :
+        if row is None :
             sql = "INSERT INTO vote (issue_id,user_id,val) VALUES (%d,%d,%d)" % (issue_id,user_id,new_val)
             c.execute(sql)
+            return sql
         else :
             old_val = row[0]
             if new_val != old_val :
                 sql = "UPDATE vote SET val=%d WHERE issue_id=%d AND user_id=%d" % (new_val,issue_id,user_id)
                 c.execute(sql)
+                return sql
     except sqlite3.Error as e:
         return "An DB error occurred:", e.args[0]
     return ""
+@app.route('/user_list')
+def user_list():
+    try:
+        c = g.db.cursor()
+        c.execute('SELECT name FROM user ORDER BY name')
+        rows = c.fetchall()
+        tmp = json.dumps(rows).decode('unicode-escape').encode('utf8').replace('], [',',')
+        return tmp[1:-1]
+    except sqlite3.Error as e:
+        return "An DB error occurred:", e.args[0]
+    
 
-@app.route('/get:_hier/')
+@app.route('/hier_html/')
 def get_hier():
+    html = '<div id="hier">'
+
+    try:
+        c = g.db.cursor()
+        sql = 'SELECT level, gr_name, name, user_id FROM user WHERE parent=0' % level
+        c.execute(sql)
+        rows = c.fetchall()
+
+    except sqlite3.Error as e:
+        return "An DB error occurred:", e.args[0]
+
+    html += '</div>'
+
     return """
 <div id="hier">
     <div class="citizen">
@@ -96,15 +152,6 @@ def get_hier():
         </div>
     </div>
 </div>"""
-
-
-@app.route('/votedlist')
-def print_something():
-    user_id = request.args.get('user_id', '')
-    bill_num = request.args.get('bill_num', '')
-    value = request.args.get('value', '')
-    app.logger.info('user_id: %s, bill_num: %s, value: %s', user_id, bill_num, value)
-    return 'hello'
 
 @app.route("/s/<path:p>")
 def handle_static(p):
