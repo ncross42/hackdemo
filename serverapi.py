2fac8800f6c746ce76f9f4a8349239274041e469
HeeWon Lee <ncross42@gmail.com>
2015-12-19 16:40:16 +0900

    bug fix app()

--- serverapi.py
+++ serverapi.py
@@ -94,5 +94,5 @@ def handle_static(p):
 
 if __name__ == "__main__":
     app.debug = True
-    #app.run(host='0.0.0.0', port=80)
-    app.run()
+    #app.run()
+    app.run(host='0.0.0.0', port=80)
