e76a58e7d4690abe8ddc97847dc8d46c602c0456
stania <stania.pe.kr@gmail.com>
2015-12-19 12:35:01 +0900

    added some static resources.

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import url_for, send_from_directory
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/s/<path:p>")
+def handle_static(p):
+    return send_from_directory("s", filename=p)
 
 if __name__ == "__main__":
     app.run()
